4cc2fa58ada27d3cae3302a11e8d5d59e5a537a1
Alex <megadonee6@gmail.com>
2020-03-11 09:44:37 +0200

    DecompiledJava

---
+++ JavaFilesDecomp/AngajatGradinaBotanica.java
@@ -0,0 +1,25 @@
+// Decompiled by DJ v3.10.10.93 Copyright 2007 Atanas Neshkov  Date: 3/11/2020 9:04:11 AM
+// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
+// Decompiler options: packimports(3) 
+// Source File Name:   AngajatGradinaBotanica.java
+
+import java.util.Vector;
+
+public class AngajatGradinaBotanica extends Persoana
+{
+
+    public AngajatGradinaBotanica()
+    {
+    }
+
+    public void cerereStergereDate()
+    {
+    }
+
+    public void cerereAdaugareDate()
+    {
+    }
+
+    public int salariu;
+    public Vector myAdministrator;
+}
