978434e62bfddc8fd7a99d0ca81040aab67861be
Alex <megadonee6@gmail.com>
2020-03-11 09:47:37 +0200

    RevEngJava

---
+++ ReverseEngJava/Planta.java
@@ -0,0 +1,54 @@
+// Decompiled by DJ v3.10.10.93 Copyright 2007 Atanas Neshkov  Date: 3/11/2020 9:03:37 AM
+// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
+// Decompiler options: packimports(3) 
+// Source File Name:   Planta.java
+
+
+public class Planta extends AngajatGradinaBotanica
+{
+
+    public Planta()
+    {
+    }
+
+    public void obtineDenumire()
+    {
+    }
+
+    public void obtineDenumireLatina()
+    {
+    }
+
+    public void obtineDescriere()
+    {
+    }
+
+    public void obtineLocatie()
+    {
+    }
+
+    public void obtineInformatii()
+    {
+    }
+
+    public void seteazaDenumire()
+    {
+    }
+
+    public void seteazaDescriere()
+    {
+    }
+
+    public void seteazaLocatie()
+    {
+    }
+
+    public void seteazaInformatii()
+    {
+    }
+
+    public String denumire;
+    public String descriere;
+    public String denumireLatina;
+    public String locatie;
+}
---
+++ ReverseEngJava/Vizitator.java
@@ -0,0 +1,31 @@
+// Decompiled by DJ v3.10.10.93 Copyright 2007 Atanas Neshkov  Date: 3/11/2020 8:59:51 AM
+// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
+// Decompiler options: packimports(3) 
+// Source File Name:   Vizitator.java
+
+
+public class Vizitator extends Persoana
+{
+
+    public Vizitator()
+    {
+    }
+
+    public void recenziePlante()
+    {
+    }
+
+    public void scanareCod()
+    {
+    }
+
+    public void adaugareFavorit()
+    {
+    }
+
+    public void obtineLocatie(Planta planta)
+    {
+    }
+
+    public int numarBilet;
+}
